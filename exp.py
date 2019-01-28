ffcd0163a0d9863173ef54de2b414cce88530674
Jeet Mohapatra <jeetjmohapatra@gmail.com>
2019-01-27 20:24:30 -0500

    Updated code to produce submission file

--- exp.py
+++ exp.py
@@ -15,7 +15,7 @@ from keras import backend as K
 df = pd.read_csv('./CMod_HackForFusion_v2.csv')
 df = df[df['intentional_disruption'] != 1.0]
 
-df_drop = df.drop(['Mirnov', 'Te_width', 'intentional_disruption', 'z_error', 'z_times_v_z', 'zcur', 'time', 'v_z'], axis=1)
+df_drop = df.drop(['Mirnov', 'Te_width', 'intentional_disruption', 'z_error', 'z_times_v_z', 'zcur', 'v_z'], axis=1)
 df_drop['time_until_disrupt'] = df_drop.time_until_disrupt.apply(lambda x: x if not math.isnan(x) else 100.0)
 
 df_clean = df_drop.dropna(how='any', axis=0)
@@ -28,11 +28,11 @@ print(len(df_clean))
 thres = 0.10
 
 def create_dataset(data, look_back=1):
-    dataX, dataY, regressY = [], [], []
+    dataX, dataY, time, shot = [], [], [], []
     for dataset in data:
         dataset = np.array(dataset)
         for i in range(len(dataset)-look_back):
-            a = dataset[i:(i+look_back), :-1]
+            a = dataset[i:(i+look_back), :-3]
             dataX.append(a)
             y = dataset[i + look_back - 1, -1]
             if math.isnan(y):
@@ -41,8 +41,9 @@ def create_dataset(data, look_back=1):
                 dataY.append(0.0)
             else:
                 dataY.append(1.0)
-            regressY.append(y)
-    return np.array(dataX), np.array(dataY), np.array(regressY)
+            time.append(dataset[i + look_back - 1, -2])
+            shot.append(dataset[i + look_back - 1, -3])
+    return np.array(dataX), np.array(dataY), np.array(time), np.array(shot)
 
 def prep_df(df):
     df_list = []
@@ -55,27 +56,37 @@ def prep_df(df):
 np.random.seed(7)
 
 
+# fix random seed for reproducibility
+np.random.seed(7)
+
+
 # split into train and test sets
 train_size = int(len(df_clean) * 0.67)
 test_size = len(df_clean) - train_size
 train, test = pd.DataFrame(df_clean[0:train_size]), pd.DataFrame(df_clean[train_size:len(df_clean)])
 t_time = train['time_until_disrupt']
 test_time = test['time_until_disrupt']
-train = train.drop(['time_until_disrupt'], axis=1)
-test = test.drop(['time_until_disrupt'], axis=1)
+train_s = train['shot']
+test_s = test['shot']
+t_t = train['time']
+test_t = test['time']
+train = train.drop(['time_until_disrupt', 'time', 'shot'], axis=1)
+test = test.drop(['time_until_disrupt', 'time', 'shot'], axis=1)
 m, std = train.mean(), train.std()
 train = (train - m)/std
 test = (test - m)/std
+train['shot'] = train_s
+test['shot'] = test_s
+train['time'] = t_t
+test['time'] = test_t
 train['time_until_disrupt'] = t_time
 test['time_until_disrupt'] = test_time
 train = prep_df(train)
 test = prep_df(test)
 
-
-# reshape into X=t and Y=t+1
 look_back = 10
-trainX, trainY, trainRegressY = create_dataset(train, look_back)
-testX, testY, testRegressY = create_dataset(test, look_back)
+trainX, trainY, timeTrain, shotTrain = create_dataset(train, look_back)
+testX, testY, timeTest, shotTest = create_dataset(test, look_back)
 
 # reshape input to be [samples, time steps, features]
 trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], trainX.shape[2]))
@@ -83,12 +94,12 @@ testX = np.reshape(testX, (testX.shape[0], testX.shape[1], testX.shape[2]))
 
 # create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(15, input_shape=(look_back, 27), return_sequences=True))
-model.add(LSTM(10))
+model.add(LSTM(10, input_shape=(look_back, 26), return_sequences=True))
+model.add(LSTM(5))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer= keras.optimizers.Adam(lr=0.0001))
-model.fit(trainX, trainY, epochs=50, batch_size=100, verbose=2)
+model.fit(trainX, trainY, epochs=2, batch_size=100, verbose=2)
 
 # make predictions
 trainPredict = model.predict(trainX)
@@ -109,3 +120,25 @@ get_2nd_layer_output = K.function([model.layers[0].input],
                                   [model.layers[1].output])
 train_features = get_2nd_layer_output([trainX])[0]
 test_features = get_2nd_layer_output([testX])[0]
+
+results = {}
+for s,t,p in zip(shotTrain, timeTrain, train_pred):
+    if p == 1.0:
+        if s not in results:
+            results[s] = t + 0.10
+
+for s,t,p in zip(shotTest, timeTest, test_pred):
+    if p == 1.0:
+        if s not in results:
+            results[s] = t + 0.10
+
+for s in set(df['shot'].unique()) - results.keys():
+    results[s] = None
+
+import csv
+
+with open('discharge_prediction.csv', mode='w') as file:
+    prediction_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    prediction_writer.writerow(['Shot_Id', 'T', 'March'])
+    for shot_id, pred_time in results.items():
+        prediction_writer.writerow([shot_id, pred_time])
